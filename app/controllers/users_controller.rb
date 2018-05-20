876cb6ee8833a241ff36dc54a2cf32fd83cee05c
Adiutant <lagynkov.rom@gmail.com>
2018-05-20 12:06:06 +0300

    1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,23 @@
 class UsersController < ApplicationController
   def index
   @a = User.all
-  #@a[0] = {id: 12134241, name: "Alex", surname: "Ivanov", email: "1@mail.ru"}
-  #@a[1] = {id: 121342141, name: "Roman", surname: "Alexeev", email: "12mail.ru"}
-  #@a[2] = {id: 121342241, name: "Vasya", surname: "Mirnov", email: "3@mail.ru"}
-  #@a[3] = {id: 121344241, name: "Senya", surname: "Dobryn", email: "11@mail.ru"}
-  #@a[4] = {id: 121343241, name: "Misha", surname: "Vasyn", email: "41@mail.ru"}
-  #@a[5] = {id: 121534241, name: "Igor", surname: "Misin", email: "14@mail.ru"}
+    #@a[0] = {id: 12134241, name: "Alex", surname: "Ivanov", email: "1@mail.ru"}
+    #@a[1] = {id: 121342141, name: "Roman", surname: "Alexeev", email: "12mail.ru"}
+    #@a[2] = {id: 121342241, name: "Vasya", surname: "Mirnov", email: "3@mail.ru"}
+    #@a[3] = {id: 121344241, name: "Senya", surname: "Dobryn", email: "11@mail.ru"}
+    #@a[4] = {id: 121343241, name: "Misha", surname: "Vasyn", email: "41@mail.ru"}
+    #@a[5] = {id: 121534241, name: "Igor", surname: "Misin", email: "14@mail.ru"}
+    #@user = User.new(name: params[:name], lastname: params[:lastname], email: params[:email])
+    #@user.save
+    #User.destroy(24)
   end
   def show 
+    #@f = User.find(2).email
   end
   def new
     @user = User.new
   end
   def create
+    @user = User.create(name: params[:user][:name], lastname: params[:user][:lastname], email: params[:user][:email],)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/index' => 'users#index'
-  get 'users/show' => 'users#show'
-  resources 'users'
+  #get 'users/index' => 'users#index'
+  #get 'users/show' => 'users#show'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
