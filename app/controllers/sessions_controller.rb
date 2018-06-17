bb6fe2859d2656b74ca137f6a2250be7e805f080
Adiutant <lagynkov.rom@gmail.com>
2018-06-17 13:25:48 +0300

    1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,12 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
+    log_in(user)
       redirect_to user
     else render "new"
     end
   end
   def destroy
+    log_out
   end
 end
